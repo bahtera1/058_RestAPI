4e82c6d68f0bf3db45946737f37b041137ba7d8c
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-28 20:35:27 +0700

    Menambahkan state dan fungsi untuk manajemen insertKontakdi InsertViewModel.kt

--- app/src/main/java/com/example/consumeapi/ui/kontak/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/home/viewmodel/InsertViewModel.kt
@@ -1,11 +1,34 @@
 package com.example.consumeapi.ui.kontak.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.consumeapi.model.Kontak
 import com.example.consumeapi.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 
-class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){}
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
 data class InsertUiEvent(
     val id: Int = 0,
     val nama: String = "",
