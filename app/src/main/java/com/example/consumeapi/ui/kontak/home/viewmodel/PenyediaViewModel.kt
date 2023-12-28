9b0128a1c909e4f2f1ed756ab235414544d3dcfc
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-12-28 20:38:18 +0700

    Menambahkan object PenyediaViewModel.kt pada PenyediaViewModel …Factory InsertViewModel.kt

--- app/src/main/java/com/example/consumeapi/ui/kontak/home/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/home/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.consumeapi.ui.kontak.home.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.consumeapi.KontakAplication
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepository)
+        }
+
+    }
+}
+fun CreationExtras.aplikasiKontak(): KontakAplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KontakAplication)
